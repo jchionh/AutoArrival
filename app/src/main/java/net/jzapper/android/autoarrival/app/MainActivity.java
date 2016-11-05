6b37e9f19ca28becdebe4c56186aa831bd03ecbe
Jason Chionh <jasonchionh@gmail.com>
2016-11-04 23:16:27 -0700

    Added controls and listview to the layout

--- app/src/main/java/net/jzapper/android/autoarrival/app/MainActivity.java
+++ app/src/main/java/net/jzapper/android/autoarrival/app/MainActivity.java
@@ -1,7 +1,9 @@
-package net.jzapper.android.autoarrival;
+package net.jzapper.android.autoarrival.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import net.jzapper.android.autoarrival.R;
 
 public class MainActivity extends AppCompatActivity
 {
